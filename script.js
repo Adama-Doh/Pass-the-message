f254d9ea95e304bc43170380e292eab6ebb08543
Samuel Adama-Doh <samueladamadoh@gmail.com>
2020-01-16 00:41:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const sendBtn = document.querySelector("#sendBtn")
+const msgIn = document.querySelector("#messageIn")
+const msgOut = document.querySelector("#messageOut")
+
+sendBtn.addEventListener('click', snedMessage);
+
+function snedMessage () {
+  let content = msgIn.value;
+  // console.log(content)
+  if(content === '') {
+    alert("Please enter a message")
+  }
+  msgOut.innerHTML = content
+  msgIn.value = ""
+  
+}
